53e2e085e0e45b403ed76ce1149e40521fa32cd3
mihailazar1 <mihailazar2222@gmail.com>
2023-11-23 20:51:44 +0200

    Recreated the project

--- android/app/src/main/kotlin/com/example/virtualmemory/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.virtualmemory
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ vmsim/android/app/src/main/kotlin/com/example/vmsim/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vmsim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
